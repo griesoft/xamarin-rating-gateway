a0996e8c68d1864f765fb53f5a490a9c7095500f
Joonas Griesinger <griesinger.joonas@gmail.com>
2020-09-20 18:42:59 +0300

    Added bool condition type.

---
+++ src/Conditions/BooleanRatingCondition.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Griesoft.Xamarin.RatingGateway.Conditions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class BooleanRatingCondition : RatingConditionBase<bool>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public BooleanRatingCondition()
+            : base(false, current => current)
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="conditionType"></param>
+        public BooleanRatingCondition(ConditionType conditionType)
+            : base(false, current => current, conditionType)
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="initialState"></param>
+        /// <param name="evaluator"></param>
+        public BooleanRatingCondition(bool initialState, Func<bool, bool> evaluator)
+            : base(initialState, evaluator)
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="initialState"></param>
+        /// <param name="evaluator"></param>
+        /// <param name="conditionType"></param>
+        public BooleanRatingCondition(bool initialState, Func<bool, bool> evaluator, ConditionType conditionType)
+            : base(initialState, evaluator, conditionType)
+        {
+
+        }
+
+        /// <inheritdoc/>
+        public override void ManipulateState()
+        {
+            CurrentState = !CurrentState;
+        }
+    }
+}
