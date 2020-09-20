2bc278570cc37a33846a730201b4ac7d8c6b7153
Joonas Griesinger <griesinger.joonas@gmail.com>
2020-09-20 18:44:13 +0300

    Added generic rating condition.

---
+++ src/Conditions/RatingCondition.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Griesoft.Xamarin.RatingGateway.Conditions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TConditionType"></typeparam>
+    public class RatingCondition<TConditionType> : RatingConditionBase<TConditionType> where TConditionType : notnull
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="initialState"></param>
+        /// <param name="evaluator"></param>
+        public RatingCondition(TConditionType initialState, Func<TConditionType, bool> evaluator)
+            : base(initialState, evaluator)
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="initialState"></param>
+        /// <param name="evaluator"></param>
+        /// <param name="conditionType"></param>
+        public RatingCondition(TConditionType initialState, Func<TConditionType, bool> evaluator, ConditionType conditionType)
+            : base(initialState, evaluator, conditionType)
+        {
+
+        }
+    }
+}
