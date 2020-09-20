ed071cf0b02ba3724ee81c462f1da4944229b5c8
Joonas Griesinger <griesinger.joonas@gmail.com>
2020-09-20 20:16:46 +0300

    Added a string matching condition.

---
+++ src/Conditions/StringMatchCondition.cs
@@ -0,0 +1,52 @@
+﻿namespace Griesoft.Xamarin.RatingGateway.Conditions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class StringMatchCondition : RatingConditionBase<string>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="goal"></param>
+        public StringMatchCondition(string goal)
+            : base(string.Empty, currentString => currentString == goal)
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="goal"></param>
+        /// <param name="conditionType"></param>
+        public StringMatchCondition(string goal, ConditionType conditionType)
+            : base(string.Empty, currentString => currentString == goal, conditionType)
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="goal"></param>
+        /// <param name="initialString"></param>
+        public StringMatchCondition(string goal, string initialString)
+            : base(initialString, currentString => currentString == goal)
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="goal"></param>
+        /// <param name="initialString"></param>
+        /// <param name="conditionType"></param>
+        public StringMatchCondition(string goal, string initialString, ConditionType conditionType)
+            : base(initialString, currentString => currentString == goal, conditionType)
+        {
+
+        }
+    }
+}
