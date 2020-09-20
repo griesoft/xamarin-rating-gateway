9704448c4bb606601c95e5db749ede334fbf3e33
Joonas Griesinger <griesinger.joonas@gmail.com>
2020-09-20 18:43:41 +0300

    Added a count rating condition.

---
+++ src/Conditions/CountRatingCondition.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Griesoft.Xamarin.RatingGateway.Conditions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CountRatingCondition : RatingConditionBase<int>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="initialState"></param>
+        /// <param name="goal"></param>
+        public CountRatingCondition(int initialState, int goal)
+            : base(initialState, currentCount => currentCount >= goal)
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="initialState"></param>
+        /// <param name="goal"></param>
+        /// <param name="conditionType"></param>
+        public CountRatingCondition(int initialState, int goal, ConditionType conditionType)
+            : base(initialState, currentCount => currentCount >= goal, conditionType)
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="initialState"></param>
+        /// <param name="evaluator"></param>
+        public CountRatingCondition(int initialState, Func<int, bool> evaluator)
+            : base(initialState, evaluator)
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="initialState"></param>
+        /// <param name="evaluator"></param>
+        /// <param name="conditionType"></param>
+        public CountRatingCondition(int initialState, Func<int, bool> evaluator, ConditionType conditionType)
+            : base(initialState, evaluator, conditionType)
+        {
+
+        }
+
+        public override void ManipulateState()
+        {
+            CurrentState++;
+        }
+    }
+}
