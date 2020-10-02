e7a2d4e4863042a4be00a0ba331fc451af172bbc
Joonas Griesinger <griesinger.joonas@gmail.com>
2020-10-02 20:50:56 +0300

    Created the interface for the condition cache and created a default implementation of it.

---
+++ src/Cache/DefaultRatingConditionCache.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Griesoft.Xamarin.RatingGateway.Helpers;
+using Newtonsoft.Json;
+
+namespace Griesoft.Xamarin.RatingGateway.Cache
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class DefaultRatingConditionCache : IRatingConditionCache
+    {
+        private readonly string _conditionCacheFileName;
+        private readonly Lazy<List<ConditionCacheDto>> _lazyConditionCacheDtos;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public DefaultRatingConditionCache() : this("RatingConditionCache") { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fileName"></param>
+        public DefaultRatingConditionCache(string fileName)
+        {
+            _conditionCacheFileName = $"{fileName}.txt";
+
+            _lazyConditionCacheDtos = new Lazy<List<ConditionCacheDto>>(() =>
+                File.Exists(Path.Combine(CacheHelpers.AppDataDirectory, _conditionCacheFileName))
+                ? JsonConvert.DeserializeObject<List<ConditionCacheDto>>(File.ReadAllText(Path.Combine(CacheHelpers.AppDataDirectory, _conditionCacheFileName)))
+                : new List<ConditionCacheDto>());
+        }
+
+        /// <inheritdoc/>
+        public void Load(string conditionName, ICachableCondition condition)
+        {
+            var cachedValueDto = _lazyConditionCacheDtos.Value.FirstOrDefault(dto => dto.ConditionName == conditionName);
+
+            if (cachedValueDto == null || cachedValueDto.CurrentValue == null)
+            {
+                return;
+            }
+
+            condition.ManipulateState(cachedValueDto.CurrentValue);
+        }
+
+        /// <inheritdoc/>
+        public void Save(string conditionName, ICachableCondition condition)
+        {
+            var existingCachedValueDto = _lazyConditionCacheDtos.Value.FirstOrDefault(dto => dto.ConditionName == conditionName);
+
+            if (existingCachedValueDto == null)
+            {
+                _lazyConditionCacheDtos.Value.Add(condition.ToConditionCacheDto(conditionName));
+            }
+            else
+            {
+                _lazyConditionCacheDtos.Value[_lazyConditionCacheDtos.Value.IndexOf(existingCachedValueDto)] = condition.ToConditionCacheDto(conditionName);
+            }
+
+            File.WriteAllText(Path.Combine(CacheHelpers.AppDataDirectory, _conditionCacheFileName), JsonConvert.SerializeObject(_lazyConditionCacheDtos.Value));
+        }
+    }
+}
---
+++ src/Interface/IRatingConditionCache.cs
@@ -0,0 +1,22 @@
+﻿namespace Griesoft.Xamarin.RatingGateway.Cache
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IRatingConditionCache
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="conditionName"></param>
+        /// <param name="condition"></param>
+        void Load(string conditionName, ICachableCondition condition);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="conditionName"></param>
+        /// <param name="condition"></param>
+        void Save(string conditionName, ICachableCondition condition);
+    }
+}
