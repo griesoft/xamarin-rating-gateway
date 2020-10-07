b796c686453a3844189da73b1ce56687118a1617
Joonas Griesinger <griesinger.joonas@gmail.com>
2020-10-07 19:42:40 +0300

    Added a sample project for Android.

---
+++ Samples/RatingGatewaySamples.Android/RatingGatewaySetup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Griesoft.Xamarin.RatingGateway;
+using Griesoft.Xamarin.RatingGateway.Conditions;
+
+namespace RatingGatewaySamples.Droid
+{
+    public static class RatingGatewaySetup
+    {
+        public static Dictionary<string, IRatingCondition> RatingConditions =>
+            new Dictionary<string, IRatingCondition>()
+            {
+                {
+                    "NeverAskAgain",
+                    new BooleanRatingCondition(false, state => !state, ConditionType.Prerequisite)
+                    {
+                        ResetAfterConditionMet = false,
+                        ExplicitManipulationOnly = true,
+                        DisallowParamaterlessManipulation = true
+                    }
+                },
+                {
+                    "CooldownBetweenRequests",
+                    new DateTimeExpiredCondition(TimeSpan.FromMinutes(1), ConditionType.Prerequisite)
+                    {
+                        ResetOnlyOnEvaluationSuccess = false
+                    }
+                },
+                {
+                    "ClickCount",
+                    new CountRatingCondition(0, 2)
+                    {
+                        ExplicitManipulationOnly = true
+                    }
+                }
+            };
+    }
+}
---
+++ Samples/RatingGatewaySamples.Android/SampleRatingView.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Android.Support.Design.Widget;
+using Android.Views;
+using Griesoft.Xamarin.RatingGateway;
+
+namespace RatingGatewaySamples.Droid
+{
+    public class SampleRatingView : IRatingView
+    {
+        private readonly View _view;
+
+        public SampleRatingView(View view)
+        {
+            _view = view;
+        }
+
+        public void TryOpenRatingPage()
+        {
+            Snackbar.Make(_view, "Opened rating page.", Snackbar.LengthLong)
+                .SetAction("Action", (View.IOnClickListener)null).Show();
+        }
+
+        public Task TryOpenRatingPageAsync()
+        {
+            Snackbar.Make(_view, "Opened rating page async.", Snackbar.LengthLong)
+                .SetAction("Action", (View.IOnClickListener)null).Show();
+
+            return Task.CompletedTask;
+        }
+    }
+}
